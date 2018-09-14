911c3090f686d19aabd82c78125e6099ddfd0eef
Damian Skowroński <damian@macbook-pro-damian.local>
2018-09-14 23:03:36 +0200

    16 minut do końca 1 części

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // MARK: Properties
+    
+    @IBOutlet weak var cardButton: UIButton!
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    var flipCardCount = 0 {
+        // observer - obserwuje wartosc i wykonuje poniższy kod po każdej zmianie wartości
+        didSet {
+            flipCountLabel.text = "Flip count: \(flipCardCount)"
+        }
+    }
+    
+    
+    // MARK: Actions
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(with_emoji: "😡", on: sender)
+        flipCardCount += 1
+    }
+    
+    // MARK: Functions
+    
+    func flipCard(with_emoji emoji: String, on button: UIButton) {
+        print("flipCardWithEmoji: \(emoji)")
+        if button.currentTitle == emoji {
+            cardButton.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+            cardButton.setTitle("", for: UIControlState.normal)
+        } else {
+            cardButton.setTitle(emoji, for: UIControlState.normal)
+            cardButton.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
